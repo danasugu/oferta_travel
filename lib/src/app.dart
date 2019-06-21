651a75021acbad39131235c3905a79c5a9a3336b
danasugu <dana.sugu@gmail.com>
2019-06-21 16:41:05 +0300

    First Commit

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
 
-class App extends StatelessWidget {
-  @override
+class App extends StatefulWidget {
+@override
+  State<StatefulWidget> createState() {
+     return AppState();
+  }
+}
+class AppState extends State<App> {
+  int counter = 0;
+
   Widget build(BuildContext context) {
     return MaterialApp (
       debugShowCheckedModeBanner: false,
@@ -12,14 +19,19 @@ class App extends StatelessWidget {
             elevation: 12.0,
             focusColor: Colors.purple,hoverColor:
             Colors.purpleAccent,
-            onPressed: () {}),
+            onPressed: () {
+          counter += 1;
+          setState(() {
+
+          });
+            }),
         bottomNavigationBar: BottomAppBar(
           color:Colors.amber,
           child: Container(height: 50.0,),
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         appBar: AppBar(backgroundColor: Colors.amber,elevation: 0.3,
-          title: Text('Hai sa vedem ofertele!'),
+          title: Text('Hai sa vedem ofertele!', style: TextStyle(fontSize: 15.5),),
           centerTitle: true,
         ),
       ),
